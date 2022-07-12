c69def37470f1bed8bb6b6da9d1bb3c2ff0f87a9
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-12 09:34:55 -0700

    task-0_printf.c and header

---
+++ main.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+
+int _printf(const char *format, ...);
---
+++ task-0_printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+* _printf - check code
+* @format: producing format
+* Use printf to print
+* Return number of character
+**/
+
+int _printf(const char *format, ...)
+{
+	va_list argu;
+	char *str;
+	char c, dlr;
+	const char *ap;
+
+	va_start(argu, format);
+	for (ap = format; *ap != '\0'; ap++)
+	{
+		switch (*ap++) {
+			case 's':              /* string */
+				str = va_arg(argu, char *);
+				while (*str !=  '\0')
+					putchar(*str++);
+			   break;
+			case '%':              /* int */
+			   dlr = va_arg(argu, int);
+			   putchar(dlr);
+			   break;
+			case 'c':
+			   c = (char) va_arg(argu, int);
+			   putchar(c);
+			   break;
+		}
+	}
+
+	va_end(argu);
+	return (*ap);
+}
+
